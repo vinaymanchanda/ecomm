f56f70833f167448ee1772bf7ae6e86738ccf042
vinaymanchanda <vmanchanda4@gmail.com>
2023-05-01 03:57:45 +0530

    backend routes

---
+++ backend/app.js
@@ -0,0 +1,29 @@
+const express= require("express");
+const ErrorHandler = require("./utils/ErrorHandler");
+const app= express();
+const cookieParser= require("cookie-parser");
+const bodyParser= require("body-parser");
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended : true}));
+app.use("/", express.static("uploads"));
+
+
+//config
+
+if(process.env.NODE_ENV !== "PRODUCTION"){
+    require("dotenv").config({
+        path:"../backend/config/.env",
+    });
+}
+
+
+//import Routes
+const user= require("./controller/user");
+app.use("/api/v2/user",user);
+
+
+//  for error handling
+app.use(ErrorHandler);
+module.exports=app;
